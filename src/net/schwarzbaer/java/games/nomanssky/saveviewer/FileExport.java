7b57958a28e8744f828233cca5a017ae260173fa
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2017-11-10 14:28:29 +0000

    JAVA: [Eclipse] - Projekt "NMS_Viewer"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/NMS_Viewer@3151 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

---
+++ src/net/schwarzbaer/java/games/nomanssky/saveviewer/FileExport.java
@@ -0,0 +1,183 @@
+package net.schwarzbaer.java.games.nomanssky.saveviewer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.ArrayValue;
+import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.BoolValue;
+import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.FloatValue;
+import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.IntegerValue;
+import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.JSON_Array;
+import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.JSON_Object;
+import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.NamedValue;
+import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.ObjectValue;
+import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.StringValue;
+import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.Value;
+
+public class FileExport {
+	
+	static void writeToJSON(JSON_Object json_Object, File copyfile) {
+		PrintWriter out;
+		try {
+			out = new PrintWriter(copyfile,StandardCharsets.UTF_8.name());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return;
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		writeToJSON_obj(out,json_Object);
+		
+		out.close();
+		
+	}
+
+	static void writeToHTML(String title, JSON_Object json_Object, File htmlfile) {
+		PrintWriter out;
+		try {
+			out = new PrintWriter(htmlfile,StandardCharsets.UTF_8.name());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return;
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("	<head>");
+		out.println("		<meta charset=\"UTF-8\">");
+		out.println("		<title>"+title+"</title>");
+		out.println("		<style type=\"text/css\" media=\"screen\">");
+		out.println("			body { font-family:sans-serif; }");
+		out.println("			.valuelist { padding-left:40px; }");
+		out.println("			.hidden { display:none; }");
+		out.println("			.button { font-weight:bold; color:#AAA; background:#EEE; }");
+		out.println("			.number { color:red; }");
+		out.println("			.string { color:#007F7F; }");
+		out.println("			.name   { color:#0000FF; }");
+		out.println("		</style>");
+		out.println("		<script type=\"text/javascript\" charset=\"utf-8\">");
+		out.println("			function toggle_collapse( btn_obj, div_ID ) {");
+		out.println("				var div_obj = document.getElementById(div_ID);");
+		out.println("				if (div_obj) {");
+		out.println("					var old_className = div_obj.className;");
+		out.println("					if (old_className == 'valuelist') {");
+		out.println("						if (btn_obj) btn_obj.innerHTML = '[+]';");
+		out.println("						div_obj.className = 'valuelist hidden';");
+		out.println("					} else {");
+		out.println("						if (btn_obj) btn_obj.innerHTML = '[-]';");
+		out.println("						div_obj.className = 'valuelist';");
+		out.println("					}");
+		out.println("				}");
+		out.println("			}");
+		out.println("		</script>");
+		out.println("	</head>");
+		out.println("<body>");
+		
+		writeToHTML_obj(out,json_Object,"obj");
+		
+		out.println("</body>");
+		out.println("</html>");
+		
+		out.close();
+	}
+
+	static void writeToJSON_obj(PrintWriter out, JSON_Object json_Object) {
+		if (json_Object==null) {
+			out.print("null");
+			return;
+		}
+		
+		out.print("{");
+		for (int i=0; i<json_Object.size(); ++i) {
+			NamedValue namedvalue = json_Object.get(i);
+			out.printf("\"%s\":",namedvalue.name);
+			writeToJSON_value(out, namedvalue.value);
+			if (i+1<json_Object.size()) out.print(",");
+		}
+		out.print("}");
+	}
+
+	static void writeToHTML_obj(PrintWriter out, JSON_Object json_Object, String ID) {
+		if (json_Object==null) {
+			out.print("null");
+			return;
+		}
+		
+		out.println("{ <span class=\"button\" onclick=\"toggle_collapse(this,'"+ID+"');\">[+]</span>");
+		out.println("<div id="+ID+" class=\"valuelist hidden\">");
+		
+		for (int i=0; i<json_Object.size(); ++i) {
+			NamedValue namedvalue = json_Object.get(i);
+			out.printf("<span class=\"name\">\"%s\"</span> : ",namedvalue.name);
+			writeToHTML_value(out, namedvalue.value, ID, i);
+			out.println(",<br/>");
+		}
+		
+		out.println("</div>");
+		out.println("}");
+	}
+
+	static void writeToJSON_arr(PrintWriter out, JSON_Array json_Array) {
+		if (json_Array==null) {
+			out.print("null");
+			return;
+		}
+		
+		out.print("[");
+		
+		for (int i=0; i<json_Array.size(); ++i) {
+			//if (json_Array.hasMixedContent()) out.print(" ");
+			writeToJSON_value(out, json_Array.get(i));
+			if (i+1<json_Array.size()) out.print(",");
+		}
+		//if (json_Array.hasMixedContent()) out.print(" ");
+		
+		out.print("]");
+	}
+
+	static void writeToHTML_arr(PrintWriter out, JSON_Array json_Array, String ID) {
+		if (json_Array==null) {
+			out.print("null");
+			return;
+		}
+		
+		out.println("[ <span class=\"button\" onclick=\"toggle_collapse(this,'"+ID+"');\">[+]</span>");
+		out.println("<div id="+ID+" class=\"valuelist hidden\">");
+		
+		for (int i=0; i<json_Array.size(); ++i) {
+			writeToHTML_value(out, json_Array.get(i), ID, i);
+			out.println(",<br/>");
+		}
+		
+		out.println("</div>");
+		out.println("]");
+	}
+
+	static void writeToJSON_value(PrintWriter out, Value value) {
+		if ( value instanceof StringValue  ) out.printf(Locale.ENGLISH,"\"%s\"", ((StringValue )value).value);
+		if ( value instanceof FloatValue   ) out.printf(Locale.ENGLISH,"%s"    , ((FloatValue  )value).value);
+		if ( value instanceof IntegerValue ) out.printf(Locale.ENGLISH,"%d"    , ((IntegerValue)value).value);
+		if ( value instanceof BoolValue    ) out.printf(Locale.ENGLISH,"%s"    , ((BoolValue   )value).value);
+		if ( value instanceof ObjectValue  ) writeToJSON_obj(out, ((ObjectValue)value).value);
+		if ( value instanceof ArrayValue   ) writeToJSON_arr(out, ((ArrayValue )value).value);
+	}
+
+	static void writeToHTML_value(PrintWriter out, Value value, String ID, int i) {
+		if ( value instanceof StringValue  ) out.printf(Locale.ENGLISH,"<span class=\"string\">"+"\"%s\""+"</span>", ((StringValue )value).value);
+		if ( value instanceof FloatValue   ) out.printf(Locale.ENGLISH,"<span class=\"number\">"+"%s"    +"</span>", ((FloatValue  )value).value);
+		if ( value instanceof IntegerValue ) out.printf(Locale.ENGLISH,"<span class=\"number\">"+"%d"    +"</span>", ((IntegerValue)value).value);
+		if ( value instanceof BoolValue    ) out.printf(Locale.ENGLISH,"<span class=\"number\">"+"%s"    +"</span>", ((BoolValue   )value).value);
+		if ( value instanceof ObjectValue  ) writeToHTML_obj(out, ((ObjectValue)value).value, ID+"_"+i);
+		if ( value instanceof ArrayValue   ) writeToHTML_arr(out, ((ArrayValue )value).value, ID+"_"+i);
+	}
+
+}
--- src/net/schwarzbaer/java/games/nomanssky/saveviewer/SaveViewer.java
+++ src/net/schwarzbaer/java/games/nomanssky/saveviewer/SaveViewer.java
@@ -1,205 +1,303 @@
 package net.schwarzbaer.java.games.nomanssky.saveviewer;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-import java.util.Locale;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Properties;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JToolBar;
+import javax.swing.JTree;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreeNode;
+
+import net.schwarzbaer.gui.Disabler;
+import net.schwarzbaer.gui.StandardMainWindow;
 import net.schwarzbaer.java.lib.jsonparser.JSON_Parser;
 import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.ArrayValue;
 import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.BoolValue;
 import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.FloatValue;
 import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.IntegerValue;
-import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.JSON_Array;
 import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.JSON_Object;
 import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.NamedValue;
 import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.ObjectValue;
 import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.StringValue;
 import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.Value;
+import net.schwarzbaer.java.lib.jsonparser.JSON_Parser.Value.Type;
 
-public class SaveViewer {
+public class SaveViewer implements ActionListener {
+
+	private static StandardMainWindow mainWindow;
+	private JSON_Object json_data;
+	private JFileChooser inputFileChooser;
+	private JFileChooser htmlFileChooser;
+	private JFileChooser jsonFileChooser;
+	private File currentFile;
+	private ContentPane contentPane;
 
 	public static void main(String[] args) {
+		new SaveViewer().createGUI();
+	}
+	
+	public SaveViewer() {
+		json_data = null;
+		currentFile = null;
+	}
+
+	private void createGUI() {
+		try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); }
+		catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {}
+		
+		inputFileChooser = new JFileChooser("./");
+		inputFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		inputFileChooser.setMultiSelectionEnabled(false);
+		inputFileChooser.addChoosableFileFilter(new FileNameExtensionFilter("No Man's Sky SaveGame (*.hg)","hg"));;
+		
+		htmlFileChooser = new JFileChooser("./");
+		htmlFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		htmlFileChooser.setMultiSelectionEnabled(false);
+		htmlFileChooser.addChoosableFileFilter(new FileNameExtensionFilter("HTML-File (*.html)","html"));;
+		
+		jsonFileChooser = new JFileChooser("./");
+		jsonFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		jsonFileChooser.setMultiSelectionEnabled(false);
+		jsonFileChooser.addChoosableFileFilter(new FileNameExtensionFilter("JSON-File (*.json)","json"));;
+		jsonFileChooser.addChoosableFileFilter(new FileNameExtensionFilter("JavaScript-File (*.js)","js"));;
+		
+		contentPane = new ContentPane();
+		
+		mainWindow = new StandardMainWindow();
+		mainWindow.startGUI(contentPane);
+		updateWindowTitle();
+	}
+	
+	private enum ActionCommand {
+		Open, WriteHTML, WriteJSON, SwitchFolder
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		ActionCommand actionCommand = ActionCommand.valueOf(e.getActionCommand());
+		switch (actionCommand) {
+		case SwitchFolder: {
+			Properties prop = System.getProperties();
+//			ArrayList<Object> keys = new ArrayList<>(prop.keySet());
+//			keys.sort(null);
+//			for (Object key : keys) {
+//				System.out.printf("[%s] = %s\r\n",key,prop.get(key));
+//			}
+			String home = prop.get("user.home").toString();
+			String fs = prop.get("file.separator").toString();
+			String path = home+fs+"AppData"+fs+"Roaming"+fs+"HelloGames"+fs+"NMS";
+			inputFileChooser.setCurrentDirectory(new File(path));
+			break;
+		}
+		case Open:
+			if (inputFileChooser.showOpenDialog(mainWindow)==JFileChooser.APPROVE_OPTION) {
+				File selectedFile = inputFileChooser.getSelectedFile();
+				JSON_Object new_json_data = new JSON_Parser(selectedFile).parse();
+				if (new_json_data!=null) {
+					currentFile = selectedFile;
+					json_data = new_json_data;
+					contentPane.tree.setModel(new DefaultTreeModel(new JsonTreeNode("root",json_data)));
+					updateWindowTitle();
+				}
+				contentPane.disabler.setEnable(ActionCommand.WriteHTML, json_data!=null);
+				contentPane.disabler.setEnable(ActionCommand.WriteJSON, json_data!=null);
+			}
+			break;
+		case WriteHTML:
+			if (json_data!=null && htmlFileChooser.showSaveDialog(mainWindow)==JFileChooser.APPROVE_OPTION) {
+				File selectedFile = htmlFileChooser.getSelectedFile();
+				FileExport.writeToHTML(currentFile.getName(),json_data,selectedFile);
+			}
+			break;
+		case WriteJSON:
+			if (json_data!=null && jsonFileChooser.showSaveDialog(mainWindow)==JFileChooser.APPROVE_OPTION) {
+				File selectedFile = jsonFileChooser.getSelectedFile();
+				FileExport.writeToJSON(json_data,selectedFile);
+			}
+			break;
+		}
+	}
+
+	private void updateWindowTitle() {
+		if (currentFile==null)
+			mainWindow.setTitle("No Man's Sky - Viewer");
+		else
+			mainWindow.setTitle("No Man's Sky - Viewer - "+currentFile.getPath());
+		System.out.println("Set window title to \""+mainWindow.getTitle()+"\"");
+	}
+
+	private class ContentPane extends JPanel {
+		private static final long serialVersionUID = -2737846401785644788L;
+		
+		private JTree tree;
+		private Disabler<ActionCommand> disabler;
+		
+		ContentPane() {
+			super( new BorderLayout(3,3) );
+			
+			disabler = new Disabler<ActionCommand>();
+			disabler.setCareFor(ActionCommand.values());
+			
+			JToolBar toolBar = new JToolBar("Standard");
+			addButtons(toolBar);
+			
+			tree = new JTree((TreeModel)null);
+			JScrollPane treeScrollPane = new JScrollPane(tree);
+			treeScrollPane.setPreferredSize(new Dimension(600, 500));
+			
+			add(toolBar,BorderLayout.PAGE_START);
+			add(treeScrollPane,BorderLayout.CENTER);
+			
+		}
+
+		private void addButtons(JToolBar toolBar) {
+			toolBar.add(createButton("Switch to NMS Savegame Folder", ActionCommand.SwitchFolder,true));
+			toolBar.add(createButton("Open Savegame", ActionCommand.Open,true));
+			toolBar.add(createButton("Write as HTML", ActionCommand.WriteHTML,false));
+			toolBar.add(createButton("Write as JSON", ActionCommand.WriteJSON,false));
+		}
+
+		private JButton createButton(String title, ActionCommand actionCommand, boolean enabled) {
+			JButton button = new JButton(title);
+			button.setActionCommand(actionCommand.toString());
+			button.addActionListener(SaveViewer.this);
+			button.setEnabled(enabled);
+			disabler.add(actionCommand, button);
+			return button;
+		}
+		
+	}
+
+	private static class JsonTreeNode implements TreeNode {
+	
+		private JsonTreeNode parent;
+		private String name;
+		private Value data;
+		private JsonTreeNode[] children;
+
+		public JsonTreeNode(String name, JSON_Object json_Object) {
+			this(null,name,new ObjectValue(json_Object));
+		}
+		
+		public JsonTreeNode(JsonTreeNode parent, String name, Value data) {
+			this.parent = parent;
+			this.name = name;
+			this.data = data;
+			this.children = null;
+		}
+
+		@Override
+		public String toString() {
+			if (name==null) return dataToString();
+			return name +" : "+ dataToString();
+		}
+
+		private String dataToString() {
+			switch(data.type) {
+			case String : return String.format("\"%s\"", ((StringValue )data).value);
+			case Bool   : return String.format("%s"    , ((BoolValue   )data).value);
+			case Float  : return                      ""+((FloatValue  )data).value ;
+			case Integer: return String.format("%d"    , ((IntegerValue)data).value);
+			case Array  : return String.format("[%d]"  , ((ArrayValue  )data).value.size());
+			case Object : return String.format("{%d}"  , ((ObjectValue )data).value.size());
+			}
+			return data.toString();
+		}
+
+		private void createChildren() {
+			switch (data.type) {
+			case Object: {
+				ObjectValue objectValue = (ObjectValue)data;
+				children = new JsonTreeNode[objectValue.value.size()];
+				int i=0;
+				for (NamedValue namedvalue : objectValue.value)
+					children[i++] = new JsonTreeNode(this,namedvalue.name,namedvalue.value);
+			} break;
+			case Array: {
+				ArrayValue arrayValue = (ArrayValue)data;
+				children = new JsonTreeNode[arrayValue.value.size()];
+				int i=0;
+				for (Value value : arrayValue.value)
+					children[i++] = new JsonTreeNode(this,null,value);
+			} break;
+			default:
+				children = new JsonTreeNode[0];
+				break;
+			
+			}
+		}
+
+		@Override
+		public TreeNode getParent() {
+			return parent;
+		}
+	
+		@Override
+		public int getChildCount() {
+			if (children == null) createChildren();
+			return children.length;
+		}
+		
+		@Override
+		public boolean isLeaf() {
+			return getChildCount()==0;
+		}
+		
+		@Override
+		public boolean getAllowsChildren() {
+			return (data.type == Type.Object) || (data.type == Type.Array);
+		}
+	
+		@Override
+		public int getIndex(TreeNode node) {
+			return Arrays.binarySearch(children, node);
+		}
+
+		@Override
+		public TreeNode getChildAt(int childIndex) {
+			return children[childIndex];
+		}
+	
+		@SuppressWarnings("rawtypes")
+		@Override
+		public Enumeration children() {
+			return new Enumeration<TreeNode>() {
+				int index = 0;
+				@Override public boolean hasMoreElements() { return children.length>index; }
+				@Override public TreeNode nextElement() { return children[index++]; }
+			};
+		}
+		
+		
+	
+	}
+
+	public static void test() {
 //		String filepath = "c:/Users/Hendrik 2/AppData/Roaming/HelloGames/NMS/st_76561198016584395/save.hg";
 		String filepath = "save.hg";
 		File sourcefile = new File(filepath);
 		
-		JSON_Parser json_Parser;
-		try {
-			json_Parser = new JSON_Parser(sourcefile);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return;
-		}
-		
-		JSON_Object json_Object = json_Parser.parse();
+		JSON_Object json_Object = new JSON_Parser(sourcefile).parse();
+		if (json_Object==null) return;
 		
 		File htmlfile = new File("./"+sourcefile.getName()+".html"); 
-		writeToHTML(sourcefile.getName(),json_Object,htmlfile);
+		FileExport.writeToHTML(sourcefile.getName(),json_Object,htmlfile);
 		
 		File copyfile = new File("./"+sourcefile.getName()+".copy.txt"); 
-		writeToJSON(json_Object,copyfile);
-	}
-
-	private static void writeToJSON(JSON_Object json_Object, File copyfile) {
-		PrintWriter out;
-		try {
-			out = new PrintWriter(copyfile,StandardCharsets.UTF_8.name());
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return;
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return;
-		}
-		
-		writeToJSON_obj(out,json_Object);
-		
-		out.close();
-		
-	}
-
-	private static void writeToHTML(String title, JSON_Object json_Object, File htmlfile) {
-		PrintWriter out;
-		try {
-			out = new PrintWriter(htmlfile,StandardCharsets.UTF_8.name());
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return;
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return;
-		}
-		
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("	<head>");
-		out.println("		<meta charset=\"UTF-8\">");
-		out.println("		<title>"+title+"</title>");
-		out.println("		<style type=\"text/css\" media=\"screen\">");
-		out.println("			body { font-family:sans-serif; }");
-		out.println("			.valuelist { padding-left:40px; }");
-		out.println("			.hidden { display:none; }");
-		out.println("			.button { font-weight:bold; color:#AAA; background:#EEE; }");
-		out.println("			.number { color:red; }");
-		out.println("			.string { color:#007F7F; }");
-		out.println("			.name   { color:#0000FF; }");
-		out.println("		</style>");
-		out.println("		<script type=\"text/javascript\" charset=\"utf-8\">");
-		out.println("			function toggle_collapse( btn_obj, div_ID ) {");
-		out.println("				var div_obj = document.getElementById(div_ID);");
-		out.println("				if (div_obj) {");
-		out.println("					var old_className = div_obj.className;");
-		out.println("					if (old_className == 'valuelist') {");
-		out.println("						if (btn_obj) btn_obj.innerHTML = '[+]';");
-		out.println("						div_obj.className = 'valuelist hidden';");
-		out.println("					} else {");
-		out.println("						if (btn_obj) btn_obj.innerHTML = '[-]';");
-		out.println("						div_obj.className = 'valuelist';");
-		out.println("					}");
-		out.println("				}");
-		out.println("			}");
-		out.println("		</script>");
-		out.println("	</head>");
-		out.println("<body>");
-		
-		writeToHTML_obj(out,json_Object,"obj");
-		
-		out.println("</body>");
-		out.println("</html>");
-		
-		out.close();
-	}
-
-	private static void writeToJSON_obj(PrintWriter out, JSON_Object json_Object) {
-		if (json_Object==null) {
-			out.print("null");
-			return;
-		}
-		
-		out.print("{");
-		for (int i=0; i<json_Object.size(); ++i) {
-			NamedValue namedvalue = json_Object.get(i);
-			out.printf("\"%s\":",namedvalue.name);
-			writeToJSON_value(out, namedvalue.value);
-			if (i+1<json_Object.size()) out.print(",");
-		}
-		out.print("}");
-	}
-
-	private static void writeToHTML_obj(PrintWriter out, JSON_Object json_Object, String ID) {
-		if (json_Object==null) {
-			out.print("null");
-			return;
-		}
-		
-		out.println("{ <span class=\"button\" onclick=\"toggle_collapse(this,'"+ID+"');\">[-]</span>");
-		out.println("<div id="+ID+" class=\"valuelist\">");
-		
-		for (int i=0; i<json_Object.size(); ++i) {
-			NamedValue namedvalue = json_Object.get(i);
-			out.printf("<span class=\"name\">\"%s\"</span> : ",namedvalue.name);
-			writeToHTML_value(out, namedvalue.value, ID, i);
-			out.println(",<br/>");
-		}
-		
-		out.println("</div>");
-		out.println("}");
-	}
-
-	private static void writeToJSON_arr(PrintWriter out, JSON_Array json_Array) {
-		if (json_Array==null) {
-			out.print("null");
-			return;
-		}
-		
-		out.print("[");
-		
-		for (int i=0; i<json_Array.size(); ++i) {
-			//if (json_Array.hasMixedContent()) out.print(" ");
-			writeToJSON_value(out, json_Array.get(i));
-			if (i+1<json_Array.size()) out.print(",");
-		}
-		//if (json_Array.hasMixedContent()) out.print(" ");
-		
-		out.print("]");
-	}
-
-	private static void writeToHTML_arr(PrintWriter out, JSON_Array json_Array, String ID) {
-		if (json_Array==null) {
-			out.print("null");
-			return;
-		}
-		
-		out.println("[ <span class=\"button\" onclick=\"toggle_collapse(this,'"+ID+"');\">[-]</span>");
-		out.println("<div id="+ID+" class=\"valuelist\">");
-		
-		for (int i=0; i<json_Array.size(); ++i) {
-			writeToHTML_value(out, json_Array.get(i), ID, i);
-			out.println(",<br/>");
-		}
-		
-		out.println("</div>");
-		out.println("]");
-	}
-
-	private static void writeToJSON_value(PrintWriter out, Value value) {
-		if ( value instanceof StringValue  ) out.printf(Locale.ENGLISH,"\"%s\"", ((StringValue )value).value);
-		if ( value instanceof FloatValue   ) out.printf(Locale.ENGLISH,"%s"    , ((FloatValue  )value).value);
-		if ( value instanceof IntegerValue ) out.printf(Locale.ENGLISH,"%d"    , ((IntegerValue)value).value);
-		if ( value instanceof BoolValue    ) out.printf(Locale.ENGLISH,"%s"    , ((BoolValue   )value).value);
-		if ( value instanceof ObjectValue  ) writeToJSON_obj(out, ((ObjectValue)value).value);
-		if ( value instanceof ArrayValue   ) writeToJSON_arr(out, ((ArrayValue )value).value);
-	}
-
-	private static void writeToHTML_value(PrintWriter out, Value value, String ID, int i) {
-		if ( value instanceof StringValue  ) out.printf(Locale.ENGLISH,"<span class=\"string\">"+"\"%s\""+"</span>", ((StringValue )value).value);
-		if ( value instanceof FloatValue   ) out.printf(Locale.ENGLISH,"<span class=\"number\">"+"%s"    +"</span>", ((FloatValue  )value).value);
-		if ( value instanceof IntegerValue ) out.printf(Locale.ENGLISH,"<span class=\"number\">"+"%d"    +"</span>", ((IntegerValue)value).value);
-		if ( value instanceof BoolValue    ) out.printf(Locale.ENGLISH,"<span class=\"number\">"+"%s"    +"</span>", ((BoolValue   )value).value);
-		if ( value instanceof ObjectValue  ) writeToHTML_obj(out, ((ObjectValue)value).value, ID+"_"+i);
-		if ( value instanceof ArrayValue   ) writeToHTML_arr(out, ((ArrayValue )value).value, ID+"_"+i);
+		FileExport.writeToJSON(json_Object,copyfile);
 	}
 }
