f5635ea9f5cb5c3cb7bcaabf9f1594859c83fc47
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2017-12-15 13:24:26 +0000

    JAVA: [Eclipse] - Projekt "NMS_Viewer"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/NMS_Viewer@3199 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/java/games/nomanssky/saveviewer/SaveViewer.java
+++ src/net/schwarzbaer/java/games/nomanssky/saveviewer/SaveViewer.java
@@ -13,8 +13,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
